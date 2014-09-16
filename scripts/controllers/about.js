ac8af0707fe37c8d82faf3190a436785e742d582
bengwinter <ben.g.winter@gmail.com>
2014-09-16 18:23:15 -0400

    working through adding carousel to about page only

--- scripts/controllers/about.js
+++ scripts/controllers/about.js
@@ -3,9 +3,13 @@
 angular
   .module('bensiteApp')
     .controller('AboutCtrl', function ($scope) {
-      $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-      ];
+      $scope.carouselInterval = 5000;
+      var slides = $scope.slides = [];
+      
+      for (var i=1; i<9; i++) {
+        slides.push({
+          image: 'https://s3.amazonaws.com/BenWinter.co/about_' + i + '.jpg',
+          text: ['Picture ' + i]
+        });
+      }
     });
