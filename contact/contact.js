190f15e565899936fba10e30baa941686ee0f92a
bengwinter <ben.g.winter@gmail.com>
2014-09-20 19:47:35 -0400

    added contact form with validation for all inputs

--- contact/contact.js
+++ contact/contact.js
@@ -3,8 +3,22 @@
 angular
   .module('bensiteApp')
     .controller('ContactCtrl', function ($scope) {
-      $scope.rightWellHeader = "Contact Me Yay";
-      $scope.rightWellText = "Placeholder Text";
-      $scope.leftWellHeader = "Contact Form Header";
-      $scope.leftWellText = "Contact Me Form";
+      $scope.WellHeader = "Contact Me";
+      $scope.WellText = "Placeholder Text";
+      $scope.submitMessage = "";
+
+      $scope.save = function() {
+          $scope.$broadcast('show-errors-check-validity');
+          
+          if ($scope.contactForm.$valid) {
+            $scope.submitMessage = 'Thank you for contacting me. I will get back to you within the next 48 hours.';
+            $scope.reset();
+          }
+        };
+        
+      $scope.reset = function() {
+        $scope.$broadcast('show-errors-reset');
+        $scope.contact = { firstName: '', lastName: '', email: '', message: '' };
+      }
+
     });
