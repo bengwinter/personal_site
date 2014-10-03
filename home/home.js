bd225d8609a06380dbacee20fdf5bb0152cc60ca
bengwinter <ben.g.winter@gmail.com>
2014-10-03 14:03:07 -0400

    populating content and designing portfolio page

--- home/home.js
+++ home/home.js
@@ -5,7 +5,8 @@ angular
     .controller('HomeCtrl', function ($scope, TextService) {
 
       TextService.getText('home.json').success(function(data){        
-        $scope.rightHeader = data[0]["rightHeader"]
+        $scope.header = data[0]["header"]
+        $scope.subHeader = data[0]["subHeader"]
         $scope.bullets = data[0]["subheaderList"]
         $scope.leftHeader = data[0]["leftHeader"]
         $scope.homePicUrl = data[0]["homePicUrl"]
--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -4,23 +4,12 @@ angular
   .module('bensiteApp')
     .controller('PortfolioCtrl', function ($scope, TextService) {
 
-
       TextService.getText('portfolio.json').success(function(data){
         $scope.header = data[0]["header"];
-        $scope.workList = data[0]["workList"];
+        $scope.positionsHeader = data[0]["positionsHeader"];
+        $scope.positions = data[0]["workList"];
+        $scope.projectsHeader = data[0]["projectsHeader"];
+        $scope.projects = data[0]["projectList"];
       });
 
-
-
-
-      $scope.carouselInterval = 5000;
-      var slides = $scope.slides = [];
-      
-      //need to update with picutres of projects
-      for (var i=1; i<9; i++) {
-        slides.push({
-          image: 'https://s3.amazonaws.com/BenWinter.co/about_' + i + '.jpg',
-          text: ['Picture ' + i]
-        });
-      }
     });
