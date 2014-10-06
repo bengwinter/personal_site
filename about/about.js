ad2ce2382763f2e6a56a5d2c17402eeba8077bf4
bengwinter <ben.g.winter@gmail.com>
2014-10-06 14:59:27 -0400

    fun facts rotation working. adding resume right now

--- about/about.js
+++ about/about.js
@@ -2,7 +2,7 @@
 
 angular
   .module('bensiteApp')
-    .controller('AboutCtrl', function ($scope, $interval, TextService) {
+    .controller('AboutCtrl', function ($scope, $timeout, TextService) {
 
       $scope.carouselInterval = 4000;
       $scope.slides = [];
@@ -12,24 +12,25 @@ angular
         $scope.rightText = data[0]["rightText"];
         $scope.slides = data[0]["carouselSlides"];
         $scope.facts = data[0]["funFacts"];
+        $scope.funFact = $scope.facts[0]["fact"];
+        $scope.resumeText = data[0]["resumeText"];
       });
 
-      // $scope.value = 0;
-      // $scope.funFact = $scope.facts[$scope.value]["fact"];
+      
+      var factNumber = 0;
 
-      // var intervalObj = $interval(function() {
-      //   $scope.value += 1;
-      //   if($scope.value == 10)
-      //   {
-      //     $interval.cancel(intervalObj);
-      //   }
-      // }, 500);
+      $scope.onTimeout = function(){
+        if (factNumber < ($scope.facts.length - 1)){
+          factNumber++;
+        } else {
+          factNumber = 0;
+        }
 
-      // $scope.funFactRotation = function() {
+        $scope.funFact = $scope.facts[factNumber]["fact"];
+        factTimeout = $timeout($scope.onTimeout,5000);
+      }
 
-      //   $scope.funFac
-        
-      // }
+      var factTimeout = $timeout($scope.onTimeout,5000);
 
       
     });
