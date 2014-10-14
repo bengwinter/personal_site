71f5ff3ed1b90d150addc7b08ec5933a97303dfb
bengwinter <ben.g.winter@gmail.com>
2014-10-14 16:52:47 -0400

    updated heroku domain link

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 var MP_TOKEN = '';
 // var CONTACT_DOMAIN = 'http://localhost:4567/mail';
-var CONTACT_DOMAIN = 'http://sendgrid-ben-site.herokuapp.com/';
+var CONTACT_DOMAIN = 'http://sendgrid-ben-site.herokuapp.com/mail';
